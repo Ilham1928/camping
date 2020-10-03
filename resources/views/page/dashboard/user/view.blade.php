9e4ea53636305c100d95080d7cc5b08b0d6201ee
vagrant <vagrant@homestead>
2020-10-03 12:34:14 +0000

    add modal and content pagination

--- resources/views/page/dashboard/user/view.blade.php
+++ resources/views/page/dashboard/user/view.blade.php
@@ -6,28 +6,32 @@
  <div class="container">
     <h3>Peralatan Camping</h3>
 	<div class="row">
-		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            @if(!empty($data['item']))
-                @foreach($data['item'] as $item)
-                    <div class="my-list">
-            			<img src="{{ asset('storage/item/' . $item['item_image']) }}" />
-            			<h3>Nama Alat : {{ $item['item_name'] }}</h3>
-            			<span>{{ $item['category']['category_name'] }}</span>
-            			<div class="offer">{{ substr($item['item_description'], 0, 50) }}..</div>
-            			<div class="detail">
-        		            <p>{{ $item['item_name'] }}</p>
-                            <img src="{{ asset('storage/item/' . $item['item_image']) }}" />
-                            <a href="#" class="btn btn-info">Pesan</a>
-                            <a href="#" class="btn btn-info">Deatil Barang</a>
-            			</div>
-            		</div>
-                @endforeach
-            @else
-                <p>Tidak Ada Peralatan</p>
-            @endif
-		</div>
+        @if(!empty($data['item']))
+            @foreach($data['item'] as $item)
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="my-list">
+        			<img style="padding:10px"     src="{{ asset('storage/item/' . $item['item_image']) }}" />
+        			<h3>Nama Alat : {{ $item['item_name'] }}</h3>
+        			<span>{{ $item['category']['category_name'] }}</span>
+        			<div class="offer">{{ substr($item['item_description'], 0, 50) }}..</div>
+        			<div class="detail">
+    		            <p>{{ $item['item_name'] }}</p>
+                        <img style="padding:10px" src="{{ asset('storage/item/' . $item['item_image']) }}" />
+                        <a href="#" class="btn btn-info">Pesan</a>
+                        <button data-toggle="modal" data-target="#myModal" class="btn btn-info">Deatil Barang</button>
+        			</div>
+        		</div>
+            </div>
+            @endforeach
+        @else
+            <p>Tidak Ada Peralatan</p>
+        @endif
 	</div>
-    <hr><br>
+    <br>
+    <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+        {{ $data['item']->links() }}
+    </div>
+    <br><br><hr><br>
     <h3>Pemandu</h3>
 	<div class="row">
 		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
@@ -51,5 +55,22 @@
             @endif
 		</div>
 	</div>
+    <br>
+    <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+        {{ $data['guide']->links() }}
+    </div>
+</div>
+
+<div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
